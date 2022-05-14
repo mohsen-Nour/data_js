baaab28ef40750e5289abc4afe59d8fb3dcdd4c4
mohsen-Nour <mohsen.noor@gmail.com>
2022-05-14 10:52:32 +0430

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-rmdjsi
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-rmdjsi)
---
+++ index.js
@@ -0,0 +1,10 @@
+var a = 1;
+function sumOfSquare(num){
+  var sum =0;
+  for(var i=0 ; i<num; i++){
+      sum = sum+Math.sqrt(i);
+  }
+  return sum;
+}
+
+
